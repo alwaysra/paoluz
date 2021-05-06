f86204ac70787eb325f3cc4bbe24ecffcb8aa120
珏氏春秋 <54929956+alwaysra@users.noreply.github.com>
2021-05-06 10:23:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# paoluz
-paoluz
+# 跑路云SSR登录并签到
+
+#### 项目介绍
+
+登录跑路云SSR 并签到使用Server酱推送运行情况
+
+#### 手动调用（仅供测试脚本是否还能用使用，定时任务不推荐）
+
+使用python3.8.2开发
+
+1. 安装依赖：
+
+```powershell
+pip install -r requirements.txt
+```
+
+2. 调用脚本：
+
+主脚本为：paoluz.py
---
+++ paoluz.py
@@ -0,0 +1,49 @@
+﻿# coding:utf-8
+import sys
+import requests
+import re
+import json
+
+loginurl = 'https://paoluz.link/auth/login'
+userurl = 'https://paoluz.link/user'
+signurl = 'https://paoluz.link/user/checkin'
+
+
+def login_sign(loginurl,userurl,userdata,signurl,SCKEY):
+    '''
+    筋斗云登录并签到
+    :param loginurl: 登录url
+    :param userurl: 用户主界面
+    :param userdata: 提交表单data
+    :param signurl: 签到url
+    :return:
+    '''
+    #登录
+    r=requests.post(url=loginurl,data=userdata)
+    if r.status_code==200:
+        print(r.json())
+        print(r.cookies)
+        cookies=r.cookies
+        #访问用户主界面
+        r=requests.get(url=userurl,cookies=cookies)
+        html=r.text
+        #使用正则表达式搜索4个卡片标签
+        counters=re.findall('<span class="counter">(.*?)</span>',html)
+        #第二个是剩余流量
+        shengyuliuliang=counters[1]
+        print('剩余流量为:'+shengyuliuliang+'GB')
+        r=requests.post(url=signurl,cookies=cookies)
+        ujson=json.loads(r.text)
+        print(ujson['msg'])
+        sendtext='签到之前剩余流量为:'+shengyuliuliang+'GB\n'+ujson['msg']
+        sendurl='https://sc.ftqq.com/' + SCKEY + '.send?text=' + sendtext
+        requests.get(sendurl)
+
+if __name__ == '__main__':
+    print(requests.__version__)
+    username=sys.argv[1]
+    userpass=sys.argv[2]
+    SCKEY = sys.argv[3]
+    userdata = {'email': username,
+                'passwd': userpass}
+    login_sign(loginurl,userurl,userdata,signurl,SCKEY)
